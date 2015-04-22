a95bb2fa82f002664b229397b5d416dedc6508b2
Peter Landwehr <pmlandwehr@gmail.com>
2015-04-22 12:01:40 -0400

    Updated docs, tweaked dumb_jpl_curler

--- dumb_jpl_curler.sh
+++ dumb_jpl_curler.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "http://memex.dyndns.org:8081/solr/imagecatdev/select?q=*%3A*&wt=json&rows=5000&page=[1-100000:5000]"
+curl "http://memex.dyndns.org:8081/solr/imagecatdev/select?q=*%3A*&wt=json&rows=5000&page=[1-100000:5000]" --output metadata_#1.json
